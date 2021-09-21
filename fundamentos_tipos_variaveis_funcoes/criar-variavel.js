fad5355ae46fe7bbccee3ce20512d0ee174668db
Rafael Dutra <rafaeldutraribas@gmail.com>
2021-09-21 15:26:33 -0300

    organização arquivos tipos var, let e const

---
+++ fundamentos_tipos_variaveis_funcoes/criar-variavel.js
@@ -0,0 +1,11 @@
+/*  Tipo que pode ser usado e dps declarado
+    Recomendado não usar
+    Será atribuido caso não declare o tipo da variavel
+*/
+var num = 5 
+
+// valor não pode ser alterado após a declaração
+const fixedNum = 15
+
+// valor mutável substitui a var
+let numIdeal = 50
--- fundamentos_tipos_variaveis_funcoes/type-number.js
+++ fundamentos_tipos_variaveis_funcoes/type-number.js
@@ -2,9 +2,4 @@
 
 const meuNumero = 3
 
-
- 
-
- 
-
 console.log(meuNumero)
